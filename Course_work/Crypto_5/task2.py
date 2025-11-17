85d8e923e370e186276365bd515e0e90f3b46969
Edvard <edvardee@stud.ntnu.no>
2025-11-17 15:41:26 +0100

    complete crypto 5

--- Course_work/Crypto_5/task2.py
+++ Course_work/Crypto_5/task2.py
@@ -1,14 +1,17 @@
 def mul_inverse(a, n):
-    for i in range(n):
-        if(a*i % n == 1): 
-            return i
+  for i in range(n):
+    if(a*i % n == 1): 
+      return i
 
-def verify(c, m, n, d):
-  residue = c**d % n
-  print(residue)
-  return residue == m
+def verify(c, m, n, e):
+  return decrypt(c, n, e) == m
 
+def encrypt(m, n, e):
+  return m ** e % n
 
+def decrypt(c, n, d):
+  return c ** d % n
+  
 p = 47
 q = 83
 e = 3
@@ -20,10 +23,19 @@ print(f"-- Task 2 - RSA --")
 print(f"1. Private key")
 d = mul_inverse(e, phi)
 print(f"k_priv (p, q, d): ({p}, {q}, {d})")
-print(f"2. Verify message")
+print(f"\n2. Verify message")
 m = 100
 c = 964
-isLegit = verify(c, m, n, d)
-print(f"Message is legit: {isLegit}")
+isLegit = verify(c, m, n, e)
+print(f"Is signature {c} valid for message {m}: {isLegit}")
+
+print(f"\n3. Encrypt message")
+
+n_B = 3127
+e_B = 33
+
+c = encrypt(m, n_B, e_B)
+
+print(f"Encrypted message {m} with public key ({n_B}, {e_B}): {c}")
 
 
